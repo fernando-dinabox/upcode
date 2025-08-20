1caff496e75c6cf09f8b5301c3d3d21cf9d39555
fernando-dinabox <fernando@dinabox.email>
2025-08-20 14:30:27 -0300

    Update upcode-init.sh

--- upcode-init.sh
+++ upcode-init.sh
@@ -2,7 +2,7 @@
 # filepath: upcode-init.sh
 
 # URL do script principal (usando a URL raw correta)
-MAIN_SCRIPT_URL="https://github.com/fernando-dinabox/upcode/blob/main/upcode-init.sh"
+MAIN_SCRIPT_URL="https://raw.githubusercontent.com/fernando-dinabox/upcode/refs/heads/main/upcode-init.sh"
 
 # Baixar e executar o script principal
 curl -s "$MAIN_SCRIPT_URL" | bash
