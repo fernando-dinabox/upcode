3d9c27ba940da8b6bad0e90a285343623234fc4c
fernando-dinabox <fernando@dinabox.email>
2025-09-02 10:09:01 -0300

    Delete upcode-init.sh

--- upcode-init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# upcode-init.sh - Sempre executa a versão mais recente
-
-echo "🚀 UPCODE - Executando versão mais recente do servidor..."
-
-# Executar diretamente sem salvar
-bash <(curl -s "https://raw.githubusercontent.com/fernando-dinabox/upcode/main/upcode-main.sh?t=$(date +%s%N)")
