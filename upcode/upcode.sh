02ae4f745188077cff285246e344c642561f61cb
fernando-dinabox <fernando@dinabox.email>
2025-09-03 16:14:17 -0300

    Update upcode.sh

--- upcode/upcode.sh
+++ upcode/upcode.sh
@@ -35,7 +35,7 @@ echo "✅ Carregamento concluido!"
 echo "Iniciando Upcode!"
 echo
 
-CURRENT_VERSION="1.1.7"
+CURRENT_VERSION="1.1.6"
 CONFIG_URL="https://db33.dev.dinabox.net/upcode/upcode.php" 
 AUTH_URL="https://db33.dev.dinabox.net/upcode/upcode.php"  
 
