669ed684e6478207b8c194045dffe67197a22c74
fernando-dinabox <fernando@dinabox.email>
2025-09-03 14:31:56 -0300

    Update u3pcode.sh

--- upcode/upcode.sh
+++ upcode/upcode.sh
@@ -35,7 +35,7 @@ echo "✅ Carregamento concluido!"
 echo "Iniciando Upcode!"
 echo
 
-CURRENT_VERSION="1.2.4"
+CURRENT_VERSION="1.2.5"
 CONFIG_URL="https://db33.dev.dinabox.net/upcode/upcode.php" 
 AUTH_URL="https://db33.dev.dinabox.net/upcode/upcode.php"  
 TOKEN_FILE="$HOME/.upcode_token"
